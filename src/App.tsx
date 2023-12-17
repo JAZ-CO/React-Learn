e8d27ed55642538d417e611637c18b0bfdd192f5
JAZ-CO <jazainaddin@gmail.com>
2023-12-17 16:31:29 +0300

    12/17/2023: Learn React day 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import ListGroup from "./components/ListGroup";
-function App(){
-  return <div><ListGroup></ListGroup></div>
+function App() {
+  let items = ["New York", "San francisco", "Tokyo"];
+  const handleSelectedItem = (item: string) => console.log(item);
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectedItem}
+      ></ListGroup>
+    </div>
+  );
 }
 
 export default App;
